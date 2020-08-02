7d3d8a6986548153f903a10d349352f707662662
Peyling Khoo <pey.khoo@united.com>
2020-08-02 01:31:06 -0500

    Updates

---
+++ precache-manifest.0c3981a5f53a2436b6cdd842537b1d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc2909ff2aa494ccaaeeea48ff8a2e0",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "52fec0b704a167b4f7ed",
+    "url": "/react-tutorial/static/css/main.f6e7e316.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fec0b704a167b4f7ed",
+    "url": "/react-tutorial/static/js/main.1275e592.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
